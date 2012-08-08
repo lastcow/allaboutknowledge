ed579841175e9eda7c6b49daa40803725754a4cb
zhijiang chen <zhijiang@chen.me>
2012-08-08 08:11:15 +0800

    Ear, Web init

---
+++ allaboutknowledge-web/src/chens/net/allaboutknowledge/web/util/Util.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package chens.net.allaboutknowledge.web.util;
+
+import java.io.Serializable;
+
+/**
+ * @author lastcow
+ *
+ */
+public class Util implements Serializable {
+
+	public String getUserName(){
+		return "陈纸浆";
+	}
+}
